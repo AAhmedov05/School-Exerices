ce050f95b7131f9586ec74b889884a9758107703
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2023-10-19 10:11:54 +0300

    Fibonaci&Evklid

---
+++ Fibbonaci Numbers/Evklid Algoritham/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Evklid_Algoritham
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Алгоритъм за намиране най-голямо общо кратно
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Evklid(a,b));
+        }
+        public static int Evklid(int a, int b)
+        {      
+            if (a==b)
+	        {
+                return a;
+    	    }
+            else
+            {
+                if (a > b)
+                {
+                    return Evklid(a-b,b);
+                }
+                else 
+                {
+                    return Evklid(a,b - a);
+                }
+            }
+        }
+    }
+}
---
+++ Fibbonaci Numbers/Fibbonaci Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibbonaci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibo(x));
+        }
+
+        public static int Fibo(int x) 
+        {
+            if (x==1||x==2)
+            {
+                return 1;
+            }
+            return Fibo(x - 2) + Fibo(x - 1);
+        }
+    }
+}
