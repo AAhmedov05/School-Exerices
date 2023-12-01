da23a03252f8939b01d26a1253b50c21a1ac00ef
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2023-12-01 12:12:14 +0200

    ASD BeforeExam

---
+++ BeforeExam/Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static string[] element;
+        static string[] comb;
+
+        public static void Combinations(int index,int start) 
+        {
+            if (index>=comb.Length)
+                Console.WriteLine($"({string.Join(',',comb)})");
+            else { 
+                for (int i = start; i < element.Length; i++)
+                {
+                    comb[index] = element[i];
+                    Combinations(index + 1,i+1);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            element = Console.ReadLine().Split(',').ToArray();
+            comb = new string[4];
+            Combinations(0,0);
+        }
+    }
+}
---
+++ BeforeExam/Exercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static string[] element;
+        static bool[] used;
+        static string[] vari;
+
+        public static void Variations(int index)
+        {
+            if (index >= vari.Length)
+                Console.WriteLine($"({string.Join(',', vari)})");
+            else
+            {
+                for (int i = 0; i < element.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        vari[index] = element[i];
+                        used[i] = true;
+                        Variations(index + 1);
+                        used[i] = false;
+                    }
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            element = Console.ReadLine().Split(',').ToArray();
+            used = new bool[element.Length];
+            vari = new string[3];
+            Variations(0);
+        }
+    }
+}
