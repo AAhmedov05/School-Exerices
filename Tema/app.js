bfe1f2815ea38d9b55c4e16a938d8463779fedc6
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2024-01-07 19:57:52 +0200

    Priemna Stranica za NPTG

--- Tema/app.js
+++ Tema/app.js
@@ -6,3 +6,6 @@ function manageMenuClick(){
    document.getElementById("mainMenu").style.display="none"
    document.getElementById("menuSpecialties").style.display="flex"
 }
+
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
