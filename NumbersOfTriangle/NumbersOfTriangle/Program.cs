3efc9ec1e4b69414efab0baf9b20f6bdd7e92db1
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2024-01-20 12:09:00 +0200

    TheNumbersOfTheTriangle

---
+++ NumbersOfTriangle/NumbersOfTriangle/Program.cs
@@ -0,0 +1,48 @@
+﻿int xCordinat = 0;
+int yCordinat = 0;
+int result = 0;
+int nextRow;
+int[,] array = {
+	{0,7,0,0,0,0 },
+	{0,3,8,0,0,0 },
+	{0,8,1,0,0,0 },
+	{0,2,7,4,4,0 },
+	{0,4,5,2,6,5 },
+};
+
+while (yCordinat<5)
+{
+	if (yCordinat==0)
+	{
+		for (int c = 0; c < 6; c++)
+		{
+			if (array[0,c]!=0)
+			{
+				result += array[0, c];
+				xCordinat= c;
+				yCordinat++;
+			}
+		}
+	}
+	else
+	{
+		for (int r = 1; r < 5; r++)
+		{
+			nextRow = array[r, xCordinat - 1];
+			if (nextRow > array[r, xCordinat + 1])
+			{
+				result += nextRow;
+				xCordinat -= 1;
+                yCordinat++;
+            }
+			else 
+			{
+				result += array[r, xCordinat + 1];
+                xCordinat += 1;
+                yCordinat++;
+            }
+			
+		}
+	}
+}
+Console.WriteLine($"Result: {result}");
