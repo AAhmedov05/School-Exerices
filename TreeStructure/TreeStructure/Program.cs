43a15b357513c0503eb2a86507336dec68b1df63
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2024-02-09 13:13:00 +0200

    ASD Tree Data Structure

---
+++ TreeStructure/TreeStructure/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree =
+                new Tree<int>(7,
+                    new Tree<int>(15,
+                        new Tree<int>(12),
+                        new Tree<int>(2),
+                        new Tree<int>(22)),
+                    new Tree<int>(13,
+                        new Tree<int>(5,
+                            new Tree<int>(23))),
+                    new Tree<int>(24)
+                    );
+
+            tree.PrintTheTree();
+        }
+           
+    }
+}
---
+++ TreeStructure/TreeStructure/Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeStructure
+{
+    public class Tree<T>
+    {
+        private T value;
+        private IList<Tree<T>> children;
+
+        public T Value { get; set; }
+        public IList<Tree<T>> Children { get; private set; }
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+
+            foreach (var child in children)
+            {
+                this.Children.Add(child);
+            }
+        }
+        public void PrintTheTree(int inception = 0)
+        {
+            Console.Write(new string(' ',2*inception));
+            Console.WriteLine(this.Value);
+            foreach (var child in this.Children)
+                child.PrintTheTree(inception + 1);
+            
+        }
+    }
+}
