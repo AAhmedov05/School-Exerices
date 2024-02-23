5b89457ebe66f4d6bfadcfc0511f599e8b3e486d
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2024-02-23 12:53:43 +0200

    DZI_ASD_EXERCISE

---
+++ TreeExercises/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreeExercises
+{
+    class Program
+    {
+        static Dictionary<int, Tree<int>> nodeByValue = new Dictionary<int, Tree<int>>();
+
+        static Tree<int> GetTreeNodeByValue(int value) 
+        {
+            if (!nodeByValue.ContainsKey(value))
+                nodeByValue[value] = new Tree<int>(value);
+            
+            return nodeByValue[value];
+        }
+
+        public static void AddEdge(int parent,int value) 
+        {
+            Tree<int> parentNode = GetTreeNodeByValue(parent);
+            Tree<int> childNode = GetTreeNodeByValue(value);
+
+            parentNode.Children.Add(childNode);
+            childNode.Parent = parentNode;
+        }
+
+        static void ReadTree() 
+        {
+            int nodeCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i < nodeCount; i++)
+            {
+                string[] edge = Console.ReadLine().Split(' ');
+                AddEdge(int.Parse(edge[0]), int.Parse(edge[1]));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ReadTree();
+            var root = nodeByValue.Values
+                .FirstOrDefault(k => k.Parent == null)
+                .Value;
+
+            var leaves = nodeByValue.Values
+                .Where(k => k.Children.Count == 0)
+                .Select(x=>x.Value)
+                .OrderBy(x=>x)
+                .ToList();
+
+            var intermediateNodes = nodeByValue.Values
+                .Where(k => k.Children.Count != 0 && k.Parent!=null)
+                .Select(x=>x.Value)
+                .OrderBy(x=>x)
+                .ToList();
+            
+            Console.WriteLine($"Root is {root}");
+            Console.WriteLine($"Leaves are {string.Join(' ',leaves)}");
+            Console.WriteLine($"Intermediate Nodes are {string.Join(' ', intermediateNodes)}");
+
+        }
+    }
+}
---
+++ TreeExercises/Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeExercises
+{
+    public class Tree<T>
+    {
+        public T Value { get; set; }
+        public Tree<T> Parent { get; set; }
+
+        public List<Tree<T>> Children { get; private set; }
+
+        public Tree(T value, params Tree<T>[] children) 
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+            foreach (var child in children)
+            {
+                this.Children.Add(child);
+                child.Parent = this;
+            }
+        }
+
+
+    }
+}
