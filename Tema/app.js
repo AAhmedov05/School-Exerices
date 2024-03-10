4df598fa7d72fc5be4e11d068ea64dcfc1728bce
AAhmedov05 <72496798+AAhmedov05@users.noreply.github.com>
2024-03-10 22:37:01 +0200

    AlmostFinished

--- Tema/app.js
+++ Tema/app.js
@@ -1,11 +1,11 @@
-const menu=document.getElementById("specialties")
+// const menu=document.getElementById("specialties")
 
-menu.addEventListener("click",manageMenuClick)
+// menu.addEventListener("click",manageMenuClick)
 
-function manageMenuClick(){
-   document.getElementById("mainMenu").style.display="none"
-   document.getElementById("menuSpecialties").style.display="flex"
-}
+// function manageMenuClick(){
+//    document.getElementById("mainMenu").style.display="none"
+//    document.getElementById("menuSpecialties").style.display="flex"
+// }
 
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
